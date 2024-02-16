bf08887079a940d5bbd8eea5fb275d334d0a6bc2
Jenifer <126200567+JeniferNatali@users.noreply.github.com>
2024-02-16 18:57:03 +0000

    Olá Mundo

---
+++ app/src/main/java/primeiro/Hello.java
@@ -0,0 +1,8 @@
+package primeiro;
+
+public class Hello {
+    public static void main (String[] args) {
+        System.out.println("Olá Mundo!");
+        System.out.println("Tchau Mundo!!");
+    }
+}
